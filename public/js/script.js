172046347cd181d680ef260a5c0830af9a709a8b
Romi Kamin <romikamin@gmail.com>
2024-02-28 10:11:00 +0200

    Implement basic registration flow

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,48 @@
-document.addEventListener('DOMContentLoaded', function(){
+function handleSearchBar() {
+  const searchButton = document.querySelector('.searchBtn');
+  const searchBar = document.querySelector('.searchBar');
+  const searchInput = document.getElementById('searchInput');
+  const searchClose = document.getElementById('searchClose');
 
-    const allButtons = document.querySelectorAll('.searchBtn');
-    const searchBar = document.querySelector('.searchBar');
-    const searchInput = document.getElementById('searchInput');
-    const searchClose = document.getElementById('searchClose');
-  
-    for (var i = 0; i < allButtons.length; i++) {
-      allButtons[i].addEventListener('click', function() {
-        searchBar.style.visibility = 'visible';
-        searchBar.classList.add('open');
-        this.setAttribute('aria-expanded', 'true');
-        searchInput.focus();
-      });
-    }
-  
-    searchClose.addEventListener('click', function() {
-      searchBar.style.visibility = 'hidden';
-      searchBar.classList.remove('open');
-      this.setAttribute('aria-expanded', 'false');
-    });
-  
-  
+  searchButton.addEventListener('click', function () {
+    searchBar.style.visibility = 'visible';
+    searchBar.classList.add('open');
+    this.setAttribute('aria-expanded', 'true');
+    searchInput.focus();
   });
+
+
+  searchClose.addEventListener('click', function () {
+    searchBar.style.visibility = 'hidden';
+    searchBar.classList.remove('open');
+    this.setAttribute('aria-expanded', 'false');
+  });
+}
+
+function handleSignupForm() {
+  const signupButton = document.querySelector('.signupBtn');
+  const signupForm = document.querySelector('.signupForm');
+  const usernameInput = document.getElementById('username');
+  const signupCancel = document.getElementById('cancelBtn');
+
+  signupButton.addEventListener('click', function () {
+    signupForm.style.visibility = 'visible';
+    signupForm.classList.add('open');
+    this.setAttribute('aria-expanded', 'true');
+    usernameInput.focus();
+  });
+
+  signupCancel.addEventListener('click', function () {
+    console.log("clicked")
+    signupForm.style.visibility = 'hidden';
+    signupForm.classList.remove('open');
+    usernameInput.value = "";
+    this.setAttribute('aria-expanded', 'false');
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  handleSearchBar()
+  handleSignupForm()
+});
+
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 /**
  * GET /
@@ -107,6 +108,35 @@ router.post('/search', async (req, res) => {
 
 });
 
+/**
+ * POST /
+ * Signup
+*/
+router.post('/signup', async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const cpassword = req.body.cpassword;
+
+  const data = await User.find({
+    username: username
+  });
+  if (data.length > 0) {
+    console.log("Username taken!")
+    return
+  }
+  if(password != cpassword) {
+    console.log("Password and confirm password are not equal!")
+    //alert("Password and confirm password are not equal!");
+    return
+  }
+  User.insertMany([{
+    username: username,
+    password: password
+  }])
+
+
+});
+
 
 
 /**
@@ -119,6 +149,53 @@ router.get('/about', (req, res) => {
   });
 });
 
+
+// function insertPostData () {
+//   Post.insertMany([
+//     {
+//       title: "Building APIs with Node.js",
+//       body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js"
+//     },
+//     {
+//       title: "Deployment of Node.js applications",
+//       body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud, and container environments..."
+//     },
+//     {
+//       title: "Authentication and Authorization in Node.js",
+//       body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js or other authentication libraries."
+//     },
+//     {
+//       title: "Understand how to work with MongoDB and Mongoose",
+//       body: "Understand how to work with MongoDB and Mongoose, an Object Data Modeling (ODM) library, in Node.js applications."
+//     },
+//     {
+//       title: "build real-time, event-driven applications in Node.js",
+//       body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js."
+//     },
+//     {
+//       title: "Discover how to use Express.js",
+//       body: "Discover how to use Express.js, a popular Node.js web framework, to build web applications."
+//     },
+//     {
+//       title: "Asynchronous Programming with Node.js",
+//       body: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for non-blocking I/O operations."
+//     },
+//     {
+//       title: "Learn the basics of Node.js and its architecture",
+//       body: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers."
+//     },
+//     {
+//       title: "NodeJs Limiting Network Traffic",
+//       body: "Learn how to limit netowrk traffic."
+//     },
+//     {
+//       title: "Learn Morgan - HTTP Request logger for NodeJs",
+//       body: "Learn Morgan."
+//     },
+//   ])
+// }
+
+// insertPostData();
 
 
 
